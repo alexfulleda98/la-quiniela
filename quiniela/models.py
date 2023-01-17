b860ffe3ced9fd2c1712482c2730273ac36e564e
Oriol Bárcenas <oriolbrcnslpz@gmail.com>
2023-01-17 14:21:02 +0100

    Fix model training and testing

--- quiniela/models.py
+++ quiniela/models.py
@@ -3,10 +3,16 @@ import numpy as np
 import json
 from sklearn import neighbors
 import warnings
+import sys
 warnings.filterwarnings("ignore")
 
 
 class QuinielaModel:
+
+    def __init__(self):
+        n_neighbors = 30
+        weights = "distance"
+        self.model = neighbors.KNeighborsClassifier(n_neighbors, weights=weights, algorithm="auto")
 
     def train(self, train_data):
         results_dic = {}
@@ -19,6 +25,10 @@ class QuinielaModel:
         x_train = train_data.copy(deep=True)
         x_train["win_diff"] = 0
         x_train["rank_diff"] = 0
+        st_index = x_train.index[0]
+        end_index = x_train.index[-1]
+        total_elems = end_index - st_index
+        curr_progress = 0
         for index, row in x_train.iterrows():
             home_team = row["home_team"]
             away_team = row["away_team"]
@@ -29,24 +39,25 @@ class QuinielaModel:
             # results_dic [team1][team2] = [win1-win2, rank1-rank2]
             x_train.loc[row.name, "win_diff"] = results_dic[home_team][away_team][0]
             x_train.loc[row.name, "rank_diff"] = results_dic[home_team][away_team][1]
+            update_progress(float(curr_progress/total_elems))
+            curr_progress += 1
         with open("train_feature_results.json", "w") as outfile:
             json.dump(results_dic, outfile)
 
         col_names = ["win_diff", "rank_diff"]
         X = x_train[col_names]
         y = x_train.results
-        n_neighbors = 30
-        weights = "distance"
-        clf = neighbors.KNeighborsClassifier(n_neighbors, weights=weights, algorithm="auto")
-        clf.fit(X, y)
-        return clf
+
+        self.model.fit(X, y)
 
     def predict(self, predict_data):
         with open("train_feature_results.json", "r") as result_input:
             results_dic = json.load(result_input)
-        predict_data["win_diff"] = 0
-        predict_data["rank_diff"] = 0
-        for index, row in predict_data.iterrows():
+        mod_predict_data = predict_data.copy(deep=True)
+        mod_predict_data["win_diff"] = 0
+        mod_predict_data["rank_diff"] = 0
+        print("progress : 'Generating Features ...'")
+        for index, row in mod_predict_data.iterrows():
             try:
                 row["home_team"]
             except KeyError:
@@ -54,16 +65,17 @@ class QuinielaModel:
             home_team = row["home_team"]
             away_team = row["away_team"]
             try:
-                predict_data.loc[row.name, "win_diff"] = results_dic[home_team][away_team][0]
-                predict_data.loc[row.name, "rank_diff"] = results_dic[home_team][away_team][1]
+                mod_predict_data.loc[row.name, "win_diff"] = results_dic[home_team][away_team][0]
+                mod_predict_data.loc[row.name, "rank_diff"] = results_dic[home_team][away_team][1]
             except KeyError:
-                predict_data.loc[row.name, "win_diff"] = 2
-                predict_data.loc[row.name, "rank_diff"] = 0
+                mod_predict_data.loc[row.name, "win_diff"] = 2
+                mod_predict_data.loc[row.name, "rank_diff"] = 0
+
 
         col_names = ["win_diff", "rank_diff"]
-        X = predict_data[col_names]
-
-        return results
+        X = mod_predict_data[col_names]
+        pred_results = self.model.predict(X)
+        return pred_results
 
     @classmethod
     def load(cls, filename):
@@ -127,3 +139,23 @@ def dic_saver(team1, team2, df, results_dic, abs_rankings):
     subdic_inv[team1] = inv_conv(direct_conf_res)
     results_dic[team1] = subdic_direct
     results_dic[team2] = subdic_inv
+
+
+def update_progress(progress):
+    barLength = 30  # Modify this to change the length of the progress bar
+    status = ""
+    if isinstance(progress, int):
+        progress = float(progress)
+    if not isinstance(progress, float):
+        progress = 0
+        status = "error: progress var must be float\r\n"
+    if progress < 0:
+        progress = 0
+        status = "Halt...\r\n"
+    if progress >= 1:
+        progress = 1
+        status = "Training the model...\r\n"
+    block = int(round(barLength*progress))
+    text = "\rProgress: [{0}] {1}% {2}".format("█"*block + "-"*(barLength-block), round(progress*100, 2), status)
+    sys.stdout.write(text)
+    sys.stdout.flush()
